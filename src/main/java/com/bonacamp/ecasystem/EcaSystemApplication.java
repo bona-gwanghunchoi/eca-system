7b05b85b1005755571f31488e5fd730658de6a74
karidong <leedh84@gmail.com>
2022-05-22 18:54:57 +0900

    initial commit

---
+++ src/main/java/com/bonacamp/ecasystem/EcaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bonacamp.ecasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcaSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bonacamp/ecasystem/EcaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonacamp.ecasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
