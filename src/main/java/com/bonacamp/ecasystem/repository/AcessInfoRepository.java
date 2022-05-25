6708af077ef7a1965e81eff0ffe851696969a670
GwanghunChoi <ghc@bona-camp.com>
2022-05-25 14:48:16 +0900

    Commit

--- src/main/java/com/bonacamp/ecasystem/repository/AcessInfoRepository.java
+++ src/main/java/com/bonacamp/ecasystem/repository/AcessInfoRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bonacamp.ecasystem.entity.AcessInfo;
 
 public interface AcessInfoRepository extends JpaRepository<AcessInfo, Long>{
-
 }
